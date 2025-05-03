a2e3db4e23619999833ab6e1671b4e5ea690ca98
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-05-03 19:46:33 +0300

    Script for choosing character name

--- scripts/name.js
+++ scripts/name.js
@@ -1,8 +1,20 @@
 const panelListElement = document.querySelector(".panel > ul");
+const chooseNameButtonElement = document.querySelector(".choose-name-btn");
+
 
 function chooseName() {
     panelListElement.style.display = "none";
+    const formsSectionElement = document.querySelector(".forms");
+    const nameInputElement = document.querySelector(".name-form input");
+    const nameFormElement = document.querySelector(".name-form");
+    formsSectionElement.style.display = "block";
+
+    nameFormElement.addEventListener("submit", (e) => {
+        e.preventDefault();
+        chooseNameButtonElement.textContent = nameInputElement.value;
+        panelListElement.style.display = "flex";
+        formsSectionElement.style.display = "none";
+    }, {once: true});
 }
 
-const chooseNameButtonElement = document.querySelector(".choose-name-btn");
 chooseNameButtonElement.addEventListener("click", chooseName);
