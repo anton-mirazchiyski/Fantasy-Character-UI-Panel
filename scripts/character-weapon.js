7ff9e6b1c682386df6996886d7c37ae3e2b15e1f
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-05-11 15:49:39 +0300

    Fix a small bug and update style
    No longer two or more weapons change style when selected
    Only one selected weapon changes it's style

--- scripts/character-weapon.js
+++ scripts/character-weapon.js
@@ -7,16 +7,22 @@ function chooseWeapon() {
     panelListElement.style.display = "none";
     weaponsSectionElement.style.display = "flex";
     
-    weaponTypes.forEach(weaponType => {
+    for (let weaponType of weaponTypes) {
         weaponType.addEventListener("click", (e) => {
             const weapon = e.target;
             chooseWeaponButtonElement.textContent = weapon.textContent;
-            // weapon.classList.remove("weapon-type");
             weapon.classList.add("chosen-weapon");
+            
             panelListElement.style.display = "flex";
             weaponsSectionElement.style.display = "none";
         });
-    });
+    };
+
+    weaponTypes.forEach(weaponType => {
+        if (weaponType.classList.contains("chosen-weapon") && weaponType.textContent != chooseWeaponButtonElement.textContent) {
+            weaponType.classList.remove("chosen-weapon");
+            }
+        });
 }
 
 chooseWeaponButtonElement.addEventListener("click", chooseWeapon);
