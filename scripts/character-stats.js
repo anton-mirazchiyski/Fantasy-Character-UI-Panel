357ba010ae3ce7ae2b86bf2a0e349cb2b87fa393
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-05-12 20:44:51 +0300

    Script for choosing main stat

---
+++ scripts/character-stats.js
@@ -0,0 +1,28 @@
+const chooseStatButtonElement = document.querySelector(".choose-mainStat-btn");
+const statsSectionElement = document.querySelector(".stats-section");
+const statButtons = document.querySelectorAll(".stat-btn");
+
+
+function chooseMainStat() {
+    panelListElement.style.display = "none";
+    statsSectionElement.style.display = "flex";
+
+    statButtons.forEach(statButton => {
+        statButton.addEventListener("click", (e) => {
+            const stat = e.target;
+            chooseStatButtonElement.textContent = stat.textContent;
+            stat.classList.add("chosen-stat");
+
+            panelListElement.style.display = "flex";
+            statsSectionElement.style.display = "none";
+        })
+    });
+
+    statButtons.forEach(statButton => {
+        if (statButton.classList.contains("chosen-stat") && statButton.textContent != chooseStatButtonElement.textContent) {
+            statButton.classList.remove("chosen-stat");
+            }
+        });
+}
+
+chooseStatButtonElement.addEventListener("click", chooseMainStat);
