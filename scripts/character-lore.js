531c5c80eb992bb6970abb825f7397dcb96fd5cc
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2026-08-18 17:16:13 +0300

    Add more phrases and update function

--- scripts/character-lore.js
+++ scripts/character-lore.js
@@ -6,7 +6,7 @@ const createCharacterButtonElement = document.querySelector('.create-character-b
 const words = {
     elementTypes: ['fire', 'lightning', 'cold', 'arcane'],
     stolenItems: ['ring', 'sword', 'gold', 'necklace'],
-    connectors: [' and ', ', moreover ', '. He '],
+    connectors: [' and ', ', moreover ', '. He ', ', he '],
     familyRelatives: ['wife', 'brother', 'sister', 'mother', 'father'],
     wealthTypes: ['noble', 'poor']
 }
@@ -16,18 +16,21 @@ const phrases = {
         'is a battle-hardened warrior',
         'sometimes makes his enemies flee from battle',
         'wields his weapon with great strength',
-        'often carries his army\'s warbanner on the battlefield'
+        'often carries his army\'s warbanner on the battlefield',
     ],
     mage: [
         'is a battle-born mage',
-        `who can strike his enemies with ${pickRandomOption(words.elementTypes)} magic from afar.`,
+        `can strike his enemies with ${pickRandomOption(words.elementTypes)} magic from afar`,
+        'is a respected wizard',
+        'is a battlemage',
     ],
     archer: [
-        'has the ability to fire multiple shots at once.',
+        'has the ability to fire multiple shots at once',
     ],
     origin: [
         `comes from a ${pickRandomOption(words.wealthTypes)} family`,
         'is a capable soldier of an empire',
+        'has endured life\'s hardships',
     ],
     purpose: [
         `seeks to return his family\'s stolen ${pickRandomOption(words.stolenItems)}`,
@@ -44,19 +47,33 @@ function constructText() {
     const meleeWeapons = [
         'Sword & Shield', 'Axe & Shield', 'Dual Swords', 'Dual Axes', 'Spear & Shield',
         'Greatsword', 'Battleaxe', 'Warhammer', 'Atgeir', 'Halberd', 'Dagger',
-    ]
+    ];
+    const mageWeapons = ['Staff'];
+    const rangedWeapons = ['Bow', 'Crossbow'];
 
-    let type = pickRandomOption(phrases.warrior);
+    const characterName = nameElement.textContent;
+    const weaponName = weaponElement.textContent;
+
+    let type;
     let origin = pickRandomOption(phrases.origin);
     let purpose = pickRandomOption(phrases.purpose);
 
+    if (meleeWeapons.includes(weaponName)) {
+        type = pickRandomOption(phrases.warrior);
+    } else if (mageWeapons.includes(weaponName)) {
+        type = pickRandomOption(phrases.mage);
+    } else if (rangedWeapons.includes(weaponName)) {
+        type = pickRandomOption(phrases.archer);
+    }
+
     let result = [
-        nameElement.textContent + ' ',
+        characterName + ' ',
         origin,
         pickRandomOption(words.connectors),
         type,
         pickRandomOption(words.connectors),
-        purpose
+        purpose,
+        '.'
     ].join('');
 
     console.log(result);
